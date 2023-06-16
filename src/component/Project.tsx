aa10cab12fab52bff4f78ab0c591db752151cd48
oNavShaHo <106837111+oNavShaHo@users.noreply.github.com>
2023-06-17 01:15:54 +0530

    Added my projectt2

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -1,64 +1,64 @@
-import React from 'react'
-import { motion } from "framer-motion"
-import quiz1 from '../images/dharma.png'
-type Props = {}
+// import React from 'react'
+// import { motion } from "framer-motion"
+// import quiz1 from '../images/dharma.png'
+// type Props = {}
 
-function Projects
-({}: Props) {
-  const projects=[1,2,3];
-  const name=["quiz","tedx","gpt"]
-  const p={
-    1:{
-      img:"",
-    },
-    2:{
-      title:"",
-    }
-  }
-  return (
-    <motion.div 
+// function Projects
+// ({}: Props) {
+//   const projects=[1,2,3];
+//   const name=["quiz","tedx","gpt"]
+//   const p={
+//     1:{
+//       img:"",
+//     },
+//     2:{
+//       title:"",
+//     }
+//   }
+//   return (
+//     <motion.div 
     
-    initial={{opacity:0}}
-    whileInView={{opacity:1}}
-    transition={{duration:1.5}}
+//     initial={{opacity:0}}
+//     whileInView={{opacity:1}}
+//     transition={{duration:1.5}}
 
-    className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify evenly mx-auto items-center z-0">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl" >
-        Projects
-      </h3>
+//     className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify evenly mx-auto items-center z-0">
+//       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl" >
+//         Projects
+//       </h3>
  
-      <div className="relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20">
-      {projects.map((project, i)=>( 
-        <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+//       <div className="relative w-full flex overflow-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+//       {projects.map((project, i)=>( 
+//         <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
         
-        <motion.img 
-          initial={{ y:-300, }} 
-          transition={{duration: 1.2}}
-          whileInView={{ opacity:1,y:0 }}
-          viewport={{ once:true }}
+//         <motion.img 
+//           initial={{ y:-300, }} 
+//           transition={{duration: 1.2}}
+//           whileInView={{ opacity:1,y:0 }}
+//           viewport={{ once:true }}
         
-         src={quiz1.src}
-          alt='yo'
+//          src={quiz1.src}
+//           alt='yo'
 
-        />
+//         />
 
-        <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-             <h4 className="text-4xl font-semibold text-center" >
-              <span className='underline decoration-[#F7AB0A]/50'>Case Study {i+1} of {projects.length}:</span> {name[i]}</h4>
+//         <div className="space-y-10 px-0 md:px-10 max-w-6xl">
+//              <h4 className="text-4xl font-semibold text-center" >
+//               <span className='underline decoration-[#F7AB0A]/50'>Case Study {i+1} of {projects.length}:</span> {name[i]}</h4>
 
-              <p className="text-lg text-center md:text-left">
-                Hello First Project of me
-              </p>
-          </div>
+//               <p className="text-lg text-center md:text-left">
+//                 Hello First Project of me
+//               </p>
+//           </div>
 
-        </div>
-      ) ) 
+//         </div>
+//       ) ) 
       
-      } 
-      </div>
-      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
-    </motion.div>
-  )
-}
+//       } 
+//       </div>
+//       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"></div>
+//     </motion.div>
+//   )
+// }
 
-export default  Projects;
+// export default  Projects;
